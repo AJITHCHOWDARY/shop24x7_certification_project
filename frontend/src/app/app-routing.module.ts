e7a2e01a92f596aa5c7f742fc54799fe1d78644f
Toheeb Bakare <toheebbakare@Toheebs-MacBook-Pro-2.local>
2021-07-27 17:07:34 -0400

    Added frontend for homepage and category listing/product detail

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminAddUserComponent } from './admin-add-user/admin-add-user.component';
 import { AdminUserUpdateComponent } from './admin-user-update/admin-user-update.component';
 import { AdminUsersComponent } from './admin-users/admin-users.component';
+import { CategoryListingComponent } from './category-listing/category-listing.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'admin-users', pathMatch:'full'},
   {path:'admin/users', component:AdminUsersComponent},
   {path:'admin/addUser', component: AdminAddUserComponent},
-  {path: 'admin/users/update/:id', component:AdminUserUpdateComponent}
+  {path: 'admin/users/update/:id', component:AdminUserUpdateComponent},
+  {path: 'homepage', component: HomepageComponent},
+  {path:'category-listing', component: CategoryListingComponent},
+  {path:'product-detail', component: ProductDetailComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,13 +9,21 @@ import { UserService } from './services/user.service';
 import { AdminAddUserComponent } from './admin-add-user/admin-add-user.component';
 import { FormsModule } from '@angular/forms';
 import { AdminUserUpdateComponent } from './admin-user-update/admin-user-update.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CategoryListingComponent } from './category-listing/category-listing.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminUsersComponent,
     AdminAddUserComponent,
-    AdminUserUpdateComponent
+    AdminUserUpdateComponent,
+    HomepageComponent,
+    CarouselComponent,
+    CategoryListingComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
